2c3e5d560353c54c754c43fe67d10fd93605fe8a
anurag <anuragbadwahe@gmail.com>
2024-02-24 00:21:10 +0530

    Added styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,28 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
+import * as React from "react";
+import { useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
 
-import EyeRelief from './EyeRelief';
-import PostureCorrection from './PostureCorrection';
-import WaterIntake from './WaterIntake';
-import BlueLightFilter from './BlueLightFilter';
 
+import EyeRelief from "./EyeRelief";
+import PostureCorrection from "./PostureCorrection";
+import WaterIntake from "./WaterIntake";
+import BlueLightFilter from "./BlueLightFilter";
 
 const cards = [
-  { title: 'Eye Relief', content: <EyeRelief /> },
-  { title: 'Posture Correction', content: <PostureCorrection/> },
-  { title: 'Water Intake Tracker', content: <WaterIntake /> },
-  { title: 'Blue light mode', content: <BlueLightFilter/> },
+  { title: "Eye Relief", content: <EyeRelief /> },
+  { title: "Posture Correction", content: <PostureCorrection /> },
+  { title: "Water Intake Tracker", content: <WaterIntake /> },
+  { title: "Blue light mode", content: <BlueLightFilter /> },
 ];
+
 
 function Main() {
   const theme = useTheme();
@@ -33,27 +34,43 @@ function Main() {
   };
 
   const handleBack = () => {
-    setActiveStep((prevActiveStep) => (prevActiveStep - 1 + maxSteps) % maxSteps);
+    setActiveStep(
+      (prevActiveStep) => (prevActiveStep - 1 + maxSteps) % maxSteps
+    );
   };
 
   return (
-    <Box sx={{ maxWidth: 600,flexGrow: 1, border: "1px solid black", borderRadius: "25%" }}>
+    <Box
+      sx={{
+        maxWidth: 600,
+        flexGrow: 1,
+        border: "1px solid black",
+        borderRadius: "25%",
+      }}
+    >
       <Paper square elevation={0}>
-        <Typography>{cards[activeStep].title}</Typography>
+        <Typography
+          sx={{
+            backgroundColor: "#0093E9",
+            backgroundImage:
+              "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)",
+            padding: "4px",
+            borderRadius: "15px",
+            fontFamily: "'Play', sans-serif", 
+          }}
+        >
+          {cards[activeStep].title}
+        </Typography>
       </Paper>
       <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
         index={activeStep}
         onChangeIndex={(index) => setActiveStep(index)}
         enableMouseEvents
       >
         {cards.map((card, index) => (
           <Box key={index}>
-            {index === activeStep && (
-              <Box>
-                {card.content}
-              </Box>
-            )}
+            {index === activeStep && <Box>{card.content}</Box>}
           </Box>
         ))}
       </SwipeableViews>
@@ -63,12 +80,20 @@ function Main() {
         activeStep={activeStep}
         nextButton={
           <Button size="small" onClick={handleNext}>
-            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
           </Button>
         }
         backButton={
           <Button size="small" onClick={handleBack}>
-            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
           </Button>
         }
       />
