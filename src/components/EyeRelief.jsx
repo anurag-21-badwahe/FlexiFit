329b02d5c0ed5a7ef56b7611ccf275a3047c79e6
anurag <anuragbadwahe@gmail.com>
2024-02-28 16:33:57 +0530

    Tried adding notification bar

--- src/components/EyeRelief.jsx
+++ src/components/EyeRelief.jsx
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import Switch from "@mui/material/Switch";
 import Slider from '@mui/material/Slider';
+import Alert from '@mui/material/Alert';
 import CountdownTimer from './CountdownTimer';
+import Notification from './Notification';
+
+function notifyUser(notificationText = "Thank you for enabling notifications!") {
+  if (!("Notification" in window)) {
+    alert("Browser does not support notifications");
+  } else if (Notification.permission === "granted") {
+    const notification = new Notification(notificationText);
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        const notification = new Notification(notificationText);
+      }
+    });
+  }
+}
 
 const notiBar = {
   display: "flex",
@@ -12,16 +28,17 @@ const notiBar = {
 
 const EyeRelief = () => {
   const [valDisp, setValDisp] = useState(false);
+  const [userResponded, setUserResponded] = useState(false);
   const [min, setMin] = useState(() => {
     const savedMin = localStorage.getItem("min");
-    return savedMin ? parseFloat(savedMin) : 20; // Retrieve min value from local storage, parse as float
+    return savedMin ? parseFloat(savedMin) : 20;
   });
   const [notificationOn, setNotificationOn] = useState(false);
   const [soundOn, setSoundOn] = useState(false);
 
   useEffect(() => {
-    if(min > 0) localStorage.setItem("min", min.toString()); // Store min value as string in local storage   
-  },[min]);
+    if (min > 0) localStorage.setItem("min", min.toString());
+  }, [min]);
 
   const onMouseEnter = () => {
     setValDisp(true);
@@ -32,27 +49,69 @@ const EyeRelief = () => {
   };
 
   const setSliderVal = (e, newVal) => {
-    if(newVal > 0){
-      setMin(newVal); // Update min value directly
+    if (newVal > 0) {
+      setMin(newVal);
     }
   };
 
   const handleNotificationToggle = () => {
-    setNotificationOn(!notificationOn);
+    setNotificationOn(prevState => !prevState);
     if (!notificationOn) {
       alert("Notifications turned on!");
     } else {
       alert("Notifications turned off!");
     }
   };
+
   const handleSoundToggle = () => {
-    setSoundOn(!soundOn);
+    setSoundOn(prevState => !prevState);
     if (!soundOn) {
-      alert("Alarm turned off!");
-    } else {
       alert("Alarm turned on!");
+    } else {
+      alert("Alarm turned off!");
     }
   };
+
+  const clickToNotify = () => {
+    console.log("clickToNotify function called");
+    if (!("Notification" in window)) {
+      console.log("Browser does not support notifications");
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      console.log("Notification permission granted");
+      showNotification();
+    } else {
+      console.log("Requesting notification permission");
+      Notification.requestPermission().then(function (permission) {
+        console.log("Permission status after request:", permission);
+        if (permission === "granted") {
+          console.log("Notification permission granted after request");
+          showNotification();
+        } else if (permission === "denied") {
+          console.log("Notification permission denied after request");
+          alert("Notifications are blocked. Please enable them in your browser settings.");
+        }
+      });
+    }
+  };
+  
+  const showNotification = () => {
+    console.log("Showing notification");
+    new Notification("Eye Relief", { body: "Take a break and rest your eyes!" });
+  };
+  
+  
+  
+  
+
+  
+  
+
+  useEffect(() => {
+    if (Notification.permission === "granted") {
+      notifyUser();
+    }
+  }, []);
 
   return (
     <>
@@ -76,11 +135,12 @@ const EyeRelief = () => {
       </div>
       <div style={notiBar}>
         <div>Sound</div>
-        <Switch onClick = {handleSoundToggle}/>
+        <Switch onClick={handleSoundToggle} />
       </div>
-      <div>You are using the device continuously for<CountdownTimer minTime={min}/></div>
+      <div>You are using the device continuously for<CountdownTimer minTime={min} /></div>
+       <button onClick={clickToNotify}>show</button>
     </>
   );
-};
+}
 
 export default EyeRelief;
